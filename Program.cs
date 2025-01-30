395ce27ced147c815080daa261bd49d6d157295d
Richi-Kodehode <richard25sfj@gmail.com>
2025-01-30 11:11:16 +0100

    added simple get value

--- Program.cs
+++ Program.cs
@@ -3,4 +3,24 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/", () =>
+{
+    return "Get from '/'";
+});
+
+app.MapPost("/", () =>
+{
+    return "Posting to '/'";
+});
+
+app.MapPut("/", () =>
+{
+    return "Updating '/'";
+});
+
+app.MapDelete("/", () =>
+{
+    return "Deleting '/'";
+});
+
 app.Run();
